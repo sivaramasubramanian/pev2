451a491ff3ba4ec59cb8ce0b23256332706f60d7
Sivarama Subramanian <sivaraam1024@gmail.com>
2022-08-31 16:13:42 +0530

    fix: fixed docs to add Github repo link (#4)

--- example/src/layouts/MainLayout.vue
+++ example/src/layouts/MainLayout.vue
@@ -14,8 +14,11 @@ const props = defineProps<Props>()
       <div v-if="props?.title" class="text-center ml-auto">
         Plan: {{ props?.title }}
       </div>
-      <v-link class="btn btn-secondary ml-auto" to="/">Home</v-link>
+      <v-link class="btn btn-link ml-auto" to="/">Home</v-link>
       <v-link class="btn btn-link" to="/about">About</v-link>
+      <a class="btn btn-link" href="https://github.com/sivaramasubramanian/pev2"
+        >Github</a
+      >
     </div>
     <slot></slot>
   </div>
